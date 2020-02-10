1aa0347d452855a82c4ccd3f3f75efcb80827f7d
Emily0701 <lijunjiang@Lijuns-MacBook-Air.local>
2020-02-10 16:30:47 -0500

    logging

---
+++ Desktop/Python/7.py
@@ -0,0 +1,17 @@
+def main():
+    name = 'Lijun'
+    print('The name is', name)
+    name = name + ' Red'
+    print('New name is', name)
+main()
+
+def main():
+    count = 0
+    my_string = input('Enter a sentence: ')
+    for ch in my_string:
+        if ch=='T' or ch=='t':
+            count +=1
+    print(f'Letter T appears {count} times.')
+main()
+
+
